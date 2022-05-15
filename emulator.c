e786479034d1016bdaabeeecbe8bb82aa7863cc0
circl <circl.lastname@gmail.com>
2022-05-15 18:24:18 +0200

    Add

---
+++ emulator.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "isa.h"
+
+uint32_t reg_a;
+uint32_t reg_b;
+uint32_t reg_x;
+uint32_t reg_s;
+
+uint8_t memory[1048576];
+
+void main(int argc, char** argv) {
+  
+}
